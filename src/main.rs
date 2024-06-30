c5e99166a072647bab59eb91ef109d24d4a692a5
lili-madeline <110143056+lili-madeline@users.noreply.github.com>
2024-06-30 22:13:22 +0000

    flac decode support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use alsa::pcm::{Access, Format, HwParams, State};
+use alsa::pcm::{Access, Format, HwParams, State, IO};
 use alsa::{Direction, ValueOr, PCM};
+use claxon::{Block, FlacReader};
+use std::env;
 use std::fs::File;
 use std::io::Read;
 
+macro_rules! dbg {
+($( $x:expr ), +) => {
+
+    if cfg!(debug_assertions) {
+        std::dbg!($($x), +)
+    } else {
+            ($($x), +)
+    }
+
+}
+}
+
 fn main() {
+    let mut track = None;
+    if env::args().len() == 2 {
+        track = env::args().last();
+    }
+    let mut reader = claxon::FlacReader::open(track.unwrap()).unwrap();
     let pcm = PCM::new("default", Direction::Playback, false).unwrap();
 
+    dbg!(reader.streaminfo());
+
     let hwp = HwParams::any(&pcm).unwrap();
-    hwp.set_channels(2).unwrap();
-    hwp.set_rate(44100, ValueOr::Nearest).unwrap();
-    hwp.set_format(Format::S32LE).unwrap();
+    hwp.set_channels(reader.streaminfo().channels).unwrap();
+    hwp.set_rate(reader.streaminfo().sample_rate, ValueOr::Nearest)
+        .unwrap();
+    hwp.set_format(match reader.streaminfo().bits_per_sample {
+        16 => Format::S16LE,
+        24 => Format::S24LE,
+        _ => panic!(),
+    })
+    .unwrap();
     hwp.set_access(Access::RWInterleaved).unwrap();
     pcm.hw_params(&hwp).unwrap();
 
@@ -21,17 +48,54 @@ fn main() {
         .unwrap();
     pcm.sw_params(&swp).unwrap();
 
-    let mut buffer = [0u8; 44100*4*2];
-    let mut f = File::open("audio_1.pcm").unwrap();
+    play_flac(reader, &pcm, &io);
+
+    pcm.drain().unwrap();
+}
+
+fn play_flac(mut reader: FlacReader<File>, pcm: &PCM, io: &IO<u8>) {
+    let mut blocks = 0;
+    let mut vec_buf = Vec::<i32>::with_capacity(reader.streaminfo().max_block_size as usize);
+    let mut block = reader.blocks().read_next_or_eof(vec_buf).unwrap();
+    loop {
+        #[cfg(debug_assertions)]
+        dbg!(blocks);
+        blocks += 1;
+        match block {
+            None => {
+                break;
+            }
+            Some(b) => {
+                let buffer = &b
+                    .stereo_samples()
+                    .map(|i| [i.0.to_le_bytes(), i.1.to_le_bytes()])
+                    .flatten()
+                    .flatten()
+                    .collect::<Vec<u8>>()[..];
+
+                dbg!(io.writei(buffer).unwrap());
+
+                vec_buf = b.into_buffer();
+                block = reader.blocks().read_next_or_eof(vec_buf).unwrap();
+                if pcm.state() != State::Running {
+                    pcm.start().unwrap();
+                };
+            }
+        }
+    }
+}
+fn play_pcm(pcm: &PCM, io: &IO<u8>) {
+    let mut buffer = [0u8; 44100];
+    let mut f = File::open("audio.pcm").unwrap();
 
     let mut time = 0;
 
-    let mut data_read =  f.read(&mut buffer).unwrap();
+    let mut data_read = f.read(&mut buffer).unwrap();
     dbg!(data_read, time);
     time += 1;
 
-    while data_read >= 44100*4*2 {
-        assert_eq!(io.writei(&buffer[..]).unwrap(), 44100);
+    while data_read >= 1024 {
+        dbg!(io.writei(&buffer[..]).unwrap());
         data_read = f.read(&mut buffer).unwrap();
         dbg!(data_read, time);
         time += 1;
@@ -39,5 +103,4 @@ fn main() {
             pcm.start().unwrap();
         };
     }
-    pcm.drain().unwrap();
 }
