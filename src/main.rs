fc369280fb67f6c35fde48dc7bd955b7ad83ca3b
lili-madeline <110143056+lili-madeline@users.noreply.github.com>
2024-07-01 02:41:08 +0000

    create and draw to window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use alsa::pcm::{Access, Format, HwParams, State, IO};
 use alsa::{Direction, ValueOr, PCM};
 use claxon::{Block, FlacReader};
-use std::env;
+use std::{env, iter};
 use std::fs::File;
 use std::io::Read;
+use std::sync::{Arc, Mutex, MutexGuard};
+use async_executor::Executor;
+use futures_lite::future;
+use wgpu::{Backends, Device, DeviceDescriptor, Extent3d, Features, Instance, InstanceDescriptor, Limits, PowerPreference, Queue, RequestAdapterOptions, Surface, TextureFormat, TextureUsages, SurfaceConfiguration, TextureViewDescriptor, CommandEncoderDescriptor, RenderPassDescriptor, RenderPassColorAttachment, Operations, LoadOp, Color, StoreOp};
+
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
+use winit::event::{WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Fullscreen, Window, WindowAttributes, WindowId};
 
 macro_rules! dbg {
 ($( $x:expr ), +) => {
@@ -17,7 +27,138 @@ macro_rules! dbg {
 }
 }
 
+#[derive(Default)]
+struct App<'a> {
+    window: Option<Arc<Window>>,
+    render: Option<Arc<Mutex<Render<'a>>>>,
+}
+
+struct Render<'a> {
+    surface: Surface<'a>,
+    device: Device,
+    queue: Queue,
+    surface_configuration: SurfaceConfiguration,
+    physical_size: PhysicalSize<u32>,
+}
+
+impl ApplicationHandler for App<'_> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(event_loop.create_window(Window::default_attributes().with_visible(true)).unwrap());
+        let executor = Executor::new();
+        self.render = Some(future::block_on(executor.run(executor.spawn(
+            wgpu_setup::<'_>(window.clone())
+        ))));
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+        let render = self.render.clone().unwrap();
+        let mut render = render.lock().unwrap();
+        match event {
+            WindowEvent::Resized(s) => {
+
+                render.physical_size = s;
+                render.surface_configuration.height = s.height;
+                render.surface_configuration.width = s.width;
+                render.surface.configure(&render.device, &render.surface_configuration);
+            }
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                let output = render.surface.get_current_texture().unwrap();
+                let view = output.texture.create_view(&TextureViewDescriptor::default());
+                let mut encoder = render.device.create_command_encoder(&CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
+                {
+                    let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                        label: Some("Render Pass"),
+                        color_attachments: &[Some(RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: Operations {
+                                load: LoadOp::Clear(Color {
+                                    r: 0.9,
+                                    g: 0.4,
+                                    b: 0.4,
+                                    a: 1.0,
+                                }),
+                                store: StoreOp::Store,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
+                    });
+                }
+                render.queue.submit(iter::once(encoder.finish()));
+                output.present();
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+async fn wgpu_setup<'a>(window: Arc<Window>) -> Arc<Mutex<Render<'a>>> {
+    let physical_size = window.inner_size();
+    let instance = Instance::new(InstanceDescriptor {
+        backends: Backends::PRIMARY,
+        ..Default::default()
+    });
+    let surface = instance.create_surface(window.clone()).unwrap();
+
+    let adapter = instance.request_adapter(&RequestAdapterOptions {
+        power_preference: PowerPreference::LowPower,
+        force_fallback_adapter: false,
+        compatible_surface: Some(&surface),
+    }).await.unwrap();
+
+    let (device, queue) = adapter.request_device(&DeviceDescriptor {
+        label: None,
+        required_features: Features::empty(),
+        required_limits: Limits::default(),
+    }, None).await.unwrap();
+
+    let surface_capabilities = surface.get_capabilities(&adapter);
+
+    let surface_format = surface_capabilities.formats.iter()
+        .find(|f| f.is_srgb())
+        .copied()
+        .unwrap_or(surface_capabilities.formats[0]);
+
+    let surface_configuration = SurfaceConfiguration {
+        usage: TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: physical_size.width,
+        height: physical_size.height,
+        present_mode: surface_capabilities.present_modes[0],
+        desired_maximum_frame_latency: 2,
+        alpha_mode: surface_capabilities.alpha_modes[0],
+        view_formats: vec![],
+    };
+
+    Arc::new(Mutex::new(Render {
+        surface,
+        device,
+        queue,
+        surface_configuration,
+        physical_size,
+    }))
+}
+
+fn run() {
+    env_logger::init();
+
+    let mut app = App::default();
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Wait);
+    event_loop.run_app(&mut app).unwrap();
+}
+
 fn main() {
+
     let mut track = None;
     if env::args().len() == 2 {
         track = env::args().last();
@@ -48,7 +189,9 @@ fn main() {
         .unwrap();
     pcm.sw_params(&swp).unwrap();
 
-    play_flac(reader, &pcm, &io);
+
+    run();
+    //play_flac(reader, &pcm, &io);
 
     pcm.drain().unwrap();
 }
